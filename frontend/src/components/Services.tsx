321405c9731dee5dfde51373c6f2c8a22f25a942
D <dracokd19@gmail.com>
2024-03-31 20:43:57 +0545

    made cards responsive

--- frontend/src/components/Services.tsx
+++ frontend/src/components/Services.tsx
@@ -4,14 +4,14 @@ function Services() {
   return (
     <div className='p-3 bg-black text-white'>
        <h1 className='text-center text-xl lg:text-2xl font-bold'>Our Services</h1>
-        <div className="grid grid-cols-4 gap-6 mx-2 mt-2">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 mx-2 mt-2">
             <div className='flex flex-col rounded-md bg-white text-black m-2 p-2'>
                 <div className='h-40 overflow-hidden rounded-t-md'>
                     <img src={paragliding} alt="" className=' object- w-full h-full'/>
                 </div>
                 <div className='p-2'>
-                    <h2 className='text-xl lg:text-2xl font-bold'>Title</h2>
-                    <p className='text-justify text-md lg:text-xl tracking-tighter'>
+                    <h2 className='text-md lg:text-2xl font-bold'>Title</h2>
+                    <p className='text-justify text-sm  lg:text-md tracking-tighter'>
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus exercitationem animi. Animi ducimus pariatur nesciunt hic, tempora sit consequatur a et sint! Quod beatae, odit culpa ducimus quasi commodi.
                     </p>
                 </div>
@@ -24,8 +24,8 @@ function Services() {
                     <img src={paragliding} alt="" className=' object- w-full h-full'/>
                 </div>
                 <div className='p-2'>
-                    <h2 className='text-xl lg:text-2xl font-bold'>Title</h2>
-                    <p className='text-justify text-md lg:text-xl tracking-tighter'>
+                    <h2 className='text-md lg:text-2xl font-bold'>Title</h2>
+                    <p className='text-justify text-sm  lg:text-md tracking-tighter'>
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus exercitationem animi. Animi ducimus pariatur nesciunt hic, tempora sit consequatur a et sint! Quod beatae, odit culpa ducimus quasi commodi.
                     </p>
                 </div>
@@ -38,8 +38,8 @@ function Services() {
                     <img src={paragliding} alt="" className=' object- w-full h-full'/>
                 </div>
                 <div className='p-2'>
-                    <h2 className='text-xl lg:text-2xl font-bold'>Title</h2>
-                    <p className='text-justify text-md lg:text-xl tracking-tighter'>
+                    <h2 className='text-md lg:text-2xl font-bold'>Title</h2>
+                    <p className='text-justify text-sm  lg:text-md tracking-tighter'>
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus exercitationem animi. Animi ducimus pariatur nesciunt hic, tempora sit consequatur a et sint! Quod beatae, odit culpa ducimus quasi commodi.
                     </p>
                 </div>
@@ -52,8 +52,8 @@ function Services() {
                     <img src={paragliding} alt="" className=' object- w-full h-full'/>
                 </div>
                 <div className='p-2'>
-                    <h2 className='text-xl lg:text-2xl font-bold'>Title</h2>
-                    <p className='text-justify text-md lg:text-xl tracking-tighter'>
+                    <h2 className='text-md lg:text-2xl font-bold'>Title</h2>
+                    <p className='text-justify text-sm  lg:text-md tracking-tighter'>
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus exercitationem animi. Animi ducimus pariatur nesciunt hic, tempora sit consequatur a et sint! Quod beatae, odit culpa ducimus quasi commodi.
                     </p>
                 </div>
