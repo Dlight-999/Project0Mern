e74b76b58838b6a2fe87b92c1e7e1940acd432ab
D <dracokd19@gmail.com>
2024-03-31 21:11:52 +0545

    fixed a bit of styling

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import background from '../assets/images/dark_background.jpg'
 function Hero() {
   return (
-    <div className='relative overflow-hidden'>
+    <div className='relative overflow-hidden sm:h-2/5'>
         <img src={background} alt="" className='w-full object-cover'/>
         <div className='absolute inset-0 flex justify-center items-center text-white bg-black/65 shadow-black shadow-lg rounded-md text-center'>
-            <div className='mx-auto'>
-                <h1 className='text-2xl md:text-5xl lg:text-6xl font-bold'>Adventure Pokhara</h1>
-                <h2 className='mt-4 text-sm md:text-xl flex'>Embark on an Adventure of a Lifetime!</h2>
+            <div className='my-auto mx-auto'>
+                <h1 className='text-xl mt-5 md:text-5xl md:mt-0 lg:text-6xl font-bold'>Adventure Pokhara</h1>
+                <h2 className='mt-4 text-sm md:text-xl md:flex'>Embark on an Adventure of a Lifetime!</h2>
                 <p className='mt-2 flex text-sm md:text-xl'>Discover Exciting Destinations and Create Lasting Memories</p>
-                <button className=" bg-sky-800 shadow-lg shadow-black p-2 px-5 mt-4 rounded-md text-xl font-semibold">
+                <button className=" bg-sky-800 shadow-lg px-2 shadow-black md:p-2 md:px-5 mt-4 rounded-md text-sm md:text-xl font-semibold">
                     Click
                 </button>
             </div>
--- frontend/src/components/Services.tsx
+++ frontend/src/components/Services.tsx
@@ -4,14 +4,14 @@ function Services() {
   return (
     <div className='p-3 bg-black text-white'>
        <h1 className='text-center text-xl lg:text-2xl font-bold'>Our Services</h1>
-        <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 mx-2 mt-2">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mx-2 mt-2">
             <div className='flex flex-col rounded-md bg-white text-black m-2 p-2'>
                 <div className='h-40 overflow-hidden rounded-t-md'>
                     <img src={paragliding} alt="" className=' object- w-full h-full'/>
                 </div>
                 <div className='p-2'>
                     <h2 className='text-md lg:text-2xl font-bold'>Title</h2>
-                    <p className='text-justify text-sm  lg:text-md tracking-tighter'>
+                    <p className='text-justify text-sm  lg:text- tracking-tighter'>
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus exercitationem animi. Animi ducimus pariatur nesciunt hic, tempora sit consequatur a et sint! Quod beatae, odit culpa ducimus quasi commodi.
                     </p>
                 </div>
