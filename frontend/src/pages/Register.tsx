7413833ca9bf01bd9881e767e31cbad46d70a96e
D <dracokd19@gmail.com>
2024-04-03 14:03:36 +0545

    .

---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import loginBg from '../assets/images/loginBg.jpg';
+import image from '../assets/images/unnamed.jpg';
+function Register() {
+  return (
+    <>
+    <div className='h-screen w-screen'>
+        <img src={loginBg} alt="" className=' object-content h-screen w-screen'/>
+        <div className='absolute inset-0 mx-auto flex justify-center items-center text-white w-screen'>
+            <div className='flex  h-auto'>
+                <div className=' bg-gray-950 flex w-full justify-center'>
+                <div className=' flex flex-col my-1 p-5'>
+                    Login form
+                    <form action="">
+                    <div className="input m-2 flex flex-col">
+                        <label htmlFor="">Username</label>
+                        <input type="text" name="" id="" />
+                    </div>
+                    <div className="input m-2 flex flex-col">
+                        <label htmlFor="">Email</label>
+                        <input type="text" name="" id="" />
+                    </div>
+                    <div className="input m-2 flex flex-col">
+                        <label htmlFor="">Password</label>
+                        <input type="text" name="" id="" />
+                    </div>
+                    <div className="input m-2 flex flex-col">
+                        <label htmlFor="">Confirm Password</label>
+                        <input type="text" name="" id="" />
+                    </div>
+                    <div className="input m-2 flex flex-col">
+                        <button>Sign up</button>
+                    </div>
+                    </form>
+                    
+                </div>
+                </div>
+                <div className='h-auto w-full'>
+                    <div className='overflow-hidden '>
+                        <img src={image} alt="" className='object-cover'/>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </>
+  )
+}
+
+export default Register
--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose  from 'mongoose';
-import router from './routes/adminRoutes.js';
+import adminRoutes from './routes/adminRoutes.js';
 const app = express();
 app.use(cors());
 app.use(express.json())
 
 mongoose.connect('mongodb://localhost:27017/project');
-app.use('/',router);
+app.use('/',adminRoutes);
 app.listen(5000,()=> console.log("server running"));
