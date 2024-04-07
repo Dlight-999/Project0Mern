3675df056404eb4ec77d24da7c0f0199792ac66b
D <dracokd19@gmail.com>
2024-04-07 20:20:54 +0545

    わかりません

--- dash/src/pages/AddActivities.tsx
+++ dash/src/pages/AddActivities.tsx
@@ -10,22 +10,29 @@ function AddActivities() {
    
     <div className='w-screen h-screen'>
     <TopBar/>
-      <div className='flex h-full flex-col justify-center items-center bg-slate-300 '>
+      <div className='flex h-full w-full flex-col justify-center items-center bg-gray-300 '>
         form and shit
         <div className=''>
           <form action="" className=''>
-           <div className='flex flex-col justify-center'>
-              <div>
+           <div className=''>
+              <div className='flex flex-col my-2'>
                 <label htmlFor="">Username</label>
-                <input type="text" name="" id="" />
+                <input type="text" name="" id="" className='w-96 p-2 rounded-md' />
               </div>
-              <div>
+              <div className='flex flex-col my-2'>
               <label htmlFor="">Email</label>
-                <input type="text" name="" id="" />
+                <input type="text" name="" id="" className='w-96 p-2  rounded-md' />
               </div>
-              <div>
+              <div className='flex flex-col my-2'>
               <label htmlFor="">Password</label>
-                <input type="text" name="" id="" />
+                <input type="text" name="" id="" className='w-96 p-2  rounded-md'/>
+              </div>
+              <div className='flex flex-col my-2'>
+              <label htmlFor="">Confirm Password</label>
+                <input type="text" name="" id="" className='w-96 p-2  rounded-md'/>
+              </div>
+              <div className='flex flex-col my-2'>
+             <button className=' bg-indigo-800 p-2 my-2 text-white rounded-md'>Add Admin</button>
               </div>
            </div>
           </form>
